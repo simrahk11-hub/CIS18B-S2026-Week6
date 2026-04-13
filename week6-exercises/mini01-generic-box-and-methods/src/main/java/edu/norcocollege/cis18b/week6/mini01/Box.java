a76a5991134e1cf63c326f61faed72e8d5e72f6e
simrahk11-hub <simrahk11@gmail.com>
2026-04-13 18:19:09 +0000

    Completed Mini 01 - Generic Box and Methods

--- week6-exercises/mini01-generic-box-and-methods/src/main/java/edu/norcocollege/cis18b/week6/mini01/Box.java
+++ week6-exercises/mini01-generic-box-and-methods/src/main/java/edu/norcocollege/cis18b/week6/mini01/Box.java
@@ -1,14 +1,16 @@
 package edu.norcocollege.cis18b.week6.mini01;
 
-public class Box<T> {
-
+public class Box <T> 
+{
     private T value;
 
-    public void set(T value) {
+    public void set(T value) 
+    {
         this.value = value;
     }
 
-    public T get() {
+    public T get() 
+    {
         return value;
     }
 }
--- week6-exercises/mini01-generic-box-and-methods/src/main/java/edu/norcocollege/cis18b/week6/mini01/GenericBoxDemo.java
+++ week6-exercises/mini01-generic-box-and-methods/src/main/java/edu/norcocollege/cis18b/week6/mini01/GenericBoxDemo.java
@@ -1,17 +1,19 @@
 package edu.norcocollege.cis18b.week6.mini01;
-
 import java.util.List;
 
 public class GenericBoxDemo {
 
-    public static <T> T first(List<T> values) {
-        if (values == null || values.isEmpty()) {
+    public static <T> T first(List<T> values) 
+    {
+        if (values == null || values.isEmpty()) 
+        {
             throw new IllegalArgumentException("values must contain at least one item");
         }
         return values.get(0);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Box<String> labelBox = new Box<>();
         labelBox.set("CIS-18B");
 
